8f92ab1f8e3e45cf7825331d977a990f6d4cc772
George <georgepavlej@gmail.com>
2023-01-21 11:44:33 +0000

    Add additional fields to admin.py for Employee, Position, Task & TaskType models

--- apps/home/admin.py
+++ apps/home/admin.py
@@ -1,8 +1,51 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-from apps.home.models import Employee, Task, TaskType, Position
+from apps.home.models import (
+    Employee,
+    Task,
+    TaskType,
+    Position,
+)
 
-admin.site.register(Employee)
-admin.site.register(Task)
-admin.site.register(TaskType)
-admin.site.register(Position)
+
+@admin.register(Employee)
+class DriverAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position", "image",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position", "image",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "license_number",
+                        "position",
+                        "image",
+                    )
+                },
+            ),
+        )
+    )
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+
+@admin.register(TaskType)
+class TypeAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("name",)
+
+
+@admin.register(Position)
+class PositionAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+    list_filter = ("name",)
